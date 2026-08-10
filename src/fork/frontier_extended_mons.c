d4a0078847405511d87c9dada14fb6719fd72e81
Gastly92 <yasinsonui@outlook.com>
2026-08-10 01:32:16 -0700

    Nidoking line review: fix both sets' Tera types and doubles legality (#413)

--- src/fork/frontier_extended_mons.c
+++ src/fork/frontier_extended_mons.c
@@ -834,7 +834,7 @@ const struct TrainerMon gFrontierExtendedMons[] =
         .moves =
         {
             MOVE_EARTH_POWER,
-            MOVE_SLUDGE_WAVE,
+            MOVE_SLUDGE_BOMB,
             MOVE_THUNDERBOLT,
             MOVE_ICE_BEAM
         },
@@ -845,11 +845,11 @@ const struct TrainerMon gFrontierExtendedMons[] =
             .spd = 4,
             .spe = 252
         ),
-        .teraType = TYPE_ICE,
+        .teraType = TYPE_WATER,
     },
     {
         .species = SPECIES_NIDOKING,
-        .tags = FORMAT_BOTH,
+        .tags = FORMAT_SINGLES,
         .heldItem = ITEM_CHOICE_SCARF,
         .moves =
         {
@@ -859,13 +859,13 @@ const struct TrainerMon gFrontierExtendedMons[] =
             MOVE_FLAMETHROWER
         },
         .ability = ABILITY_SHEER_FORCE,
-        .nature = NATURE(SPE_UP, ATK_DOWN),
+        .nature = NATURE(SPA_UP, ATK_DOWN),
         .ev = EVS(
             .spa = 252,
             .spd = 4,
             .spe = 252
         ),
-        .teraType = TYPE_FIRE,
+        .teraType = TYPE_GHOST,
     },
 
     // 0036
